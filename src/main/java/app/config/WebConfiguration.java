a2ef982ce5497a5c92e0a22745930cdfc9f8096a
YuriyAnat <anatienko.yv@gmail.com>
2019-03-05 20:51:15 +0300

    version 0.4

--- src/main/java/app/config/WebConfiguration.java
+++ src/main/java/app/config/WebConfiguration.java
@@ -1,10 +1,10 @@
 package app.config;
 
-import app.connections.HibernateConfiguration;
+import app.models.User;
 import org.hibernate.SessionFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -14,7 +14,11 @@ import org.springframework.web.servlet.view.JstlView;
 @Configuration
 @EnableWebMvc
 @ComponentScan("app")
+@PropertySource("classpath:config.properties")
 public class WebConfiguration extends WebMvcConfigurerAdapter {
+    @Autowired
+    private Environment env;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
@@ -31,7 +35,18 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
     }
 
     @Bean
+    @Scope("singleton")
     public SessionFactory getSessionFactory(){
-        return new HibernateConfiguration().getConfiguration().buildSessionFactory();
+        System.err.println(env.getProperty("host"));
+        org.hibernate.cfg.Configuration configuration = new org.hibernate.cfg.Configuration();
+        configuration.setProperty("hibernate.dialect", env.getProperty("dialect"));
+        configuration.setProperty("hibernate.connection.driver_class", env.getProperty("driver"));
+        configuration.setProperty("hibernate.connection.url",env.getProperty("url") + env.getProperty("host") + ":" + env.getProperty("port") + "/" + env.getProperty("base"));
+        configuration.setProperty("hibernate.connection.username", env.getProperty("user"));
+        configuration.setProperty("hibernate.connection.password", env.getProperty("password"));
+        configuration.setProperty("hibernate.show_sql", env.getProperty("show_sql"));
+        configuration.setProperty("hibernate.hbm2ddl.auto", env.getProperty("hbm2ddl"));
+        configuration.addAnnotatedClass(User.class);
+        return configuration.buildSessionFactory();
     }
 }
