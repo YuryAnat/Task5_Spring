7133257ce7717cb634c6d5c63350e96ff1b9c7ae
YuriyAnat <anatienko.yv@gmail.com>
2019-03-12 20:50:25 +0300

    version 0.8

--- src/main/java/app/security/SuccessHandler.java
+++ src/main/java/app/security/SuccessHandler.java
@@ -1,15 +1,13 @@
-package app.config;
+package app.security;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
-import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Service;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.Collection;
 
--- src/main/java/app/security/WebSecurityConfiguration.java
+++ src/main/java/app/security/WebSecurityConfiguration.java
@@ -1,4 +1,4 @@
-package app.config;
+package app.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
